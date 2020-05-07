f3d626e4871bd6b1eaf3b2827d558e747bc9710c
Hanzala Sohrab <sorb.hanzala@gmail.com>
2020-05-08 01:49:10 +0530

    A+B (Trial Problem)

---
+++ Testing Round #16 (Unrated)/A.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main () {
+	cin.sync_with_stdio(false);
+	cin.tie(0);
+	int t, a, b;
+	cin >> t;
+	while (t--) {
+		cin >> a >> b;
+		cout << a + b << '\n';
+	}
+	return 0;
+}
