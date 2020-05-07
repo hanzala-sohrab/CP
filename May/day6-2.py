dc6ec364f4991be8b78247d40df911a590538e39
Hanzala Sohrab <40603380+hanzala-sohrab@users.noreply.github.com>
2020-05-07 15:26:09 +0530

    Delete day6-2.py

--- May/day6-2.py
+++
@@ -1,8 +0,0 @@
-# Brute force - O(n2)
-class Solution:
-    def majorityElement(self, nums):
-        majority_count = len(nums)//2
-        for num in nums:
-            count = sum(1 for elem in nums if elem == num)
-            if count > majority_count:
-                return num
