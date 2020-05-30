3843e5edcd6b281eeedf1673b120f2bb24e30c39
Hanzala Sohrab <sorb.hanzala@gmail.com>
2020-05-31 02:47:05 +0530

    Reverse The Number

---
+++ LRNDSA01/FLOW007.cpp
@@ -0,0 +1,47 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mmax(a,b,c) max(a,max(b,c))
+#define mmin(a,b,c) min(a,min(b,c))
+#define M 1000000007
+#define V vector
+#define F(a,b,c) for (a=b;a<=c;++a)
+#define I cin
+#define O cout
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ff first
+#define ss second
+
+ll binpow(ll a, ll b) {
+    a %= M;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % M;
+        a = a * a % M;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    I.sync_with_stdio(false);
+    I.tie(0);
+    ll t,n,q,i,j,k;
+    I>>t;
+    while (t--)
+    {
+        I>>n;
+        q = n;
+        i = 0;
+        while (q)
+        {
+            i *= 10;
+            i += q % 10;
+            q /= 10;
+        }
+        O << i << '\n';
+    }
+    return 0;
+}
