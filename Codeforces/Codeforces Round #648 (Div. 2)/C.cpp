bca8cd25a70d78838811b6208842ca9d8d3f35f9
Hanzala Sohrab <sorb.hanzala@gmail.com>
2020-06-10 16:18:55 +0530

    Rotation Matching

---
+++ Codeforces/Codeforces Round #648 (Div. 2)/C.cpp
@@ -0,0 +1,67 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mmax(a,b,c) max(a,max(b,c))
+#define mmin(a,b,c) min(a,min(b,c))
+#define M 1000000007
+#define V vector
+#define F(a,b,c) for (a=b;a<=c;++a)
+#define I cin
+#define O cout
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ff first
+#define ss second
+
+bool isPowerOfTwo (ll x) {
+    return x && (!(x&(x-1)));
+}
+
+ll binpow(ll a, ll b) {
+    a %= M;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % M;
+        a = a * a % M;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    I.sync_with_stdio(false);
+    I.tie(0);
+    ll t,n,q,i,j,k;
+    // I>>t;
+    t = 1;
+    while (t--)
+    {
+        I >> n;
+        V<ll> a(n), b(n);
+        F (i, 0, n - 1)
+            I >> a[i];
+        F (i, 0, n - 1)
+            I >> b[i];
+        ll m = 0, c;
+        F (i, 0, n - 1)
+        {
+            F (j, 0, n - 1)
+            {
+                if (b[j] == a[i])
+                {
+                    k = j - i;
+                    c = 0;
+                    F (q, 0, n - 1)
+                    {
+                        if (a[q] == b[(q + k) % n])
+                            ++c;
+                    }
+                    m = max(m, c);
+                }
+            }
+        }
+        O << m << '\n';
+    }
+    return 0;
+}
