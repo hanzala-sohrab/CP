ee5300296eec027842c3613fb7e00e4780823fab
Hanzala Sohrab <sorb.hanzala@gmail.com>
2020-06-10 19:23:46 +0530

    Rotation Matching

--- Codeforces/Codeforces Round #648 (Div. 2)/C.cpp
+++ Codeforces/Codeforces Round #648 (Div. 2)/C.cpp
@@ -17,34 +17,28 @@ int main() {
     I.sync_with_stdio(false);
     I.tie(0);
     ll t,n,q,i,j,k;
-    // I>>t;
     t = 1;
     while (t--)
     {
         I >> n;
-        V<ll> a(n), b(n);
+        V<ll> a(n), b(n), p(n + 1);
+        unordered_map<ll, ll> f;
         F (i, 0, n - 1)
+        {
             I >> a[i];
+            p[a[i]] = i;
+        }
         F (i, 0, n - 1)
             I >> b[i];
         ll m = 0, c;
         F (i, 0, n - 1)
         {
-            F (j, 0, n - 1)
-            {
-                if (b[j] == a[i])
-                {
-                    k = j - i;
-                    c = 0;
-                    F (q, 0, n - 1)
-                    {
-                        if (a[q] == b[(q + k) % n])
-                            ++c;
-                    }
-                    m = max(m, c);
-                }
-            }
+            c = i - p[b[i]];
+            c = c < 0 ? c + n : c;
+            f[c]++;
         }
+        for (auto it: f)
+            m = max(m, it.ss);
         O << m << '\n';
     }
     return 0;
