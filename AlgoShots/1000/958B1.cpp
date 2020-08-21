0106fb20e7868208fa1e048e0eec41da0ced8028
Hanzala Sohrab <sorb.hanzala@gmail.com>
2020-08-21 15:23:01 +0530

    958B1 - Maximum Control (easy)

---
+++ AlgoShots/1000/958B1.cpp
@@ -0,0 +1,51 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mmax(a,b,c) max(a,max(b,c))
+#define mmin(a,b,c) min(a,min(b,c))
+#define M 1000000007
+#define V vector
+#define F(a,b,c) for (a=b;a<=c;++a)
+#define I cin
+#define O cout
+#define pb push_back
+#define all(x) x.begin(),x.end()
+#define ff first
+#define ss second
+#define umap unordered_map
+
+bool isPowerOfTwo (ll x) {
+    return x && (!(x&(x-1)));
+}
+
+ll binpow(ll a, ll b) {
+    a %= M;
+    ll res = 1;
+    while (b > 0) {
+        if (b & 1)
+            res = res * a % M;
+        a = a * a % M;
+        b >>= 1;
+    }
+    return res;
+}
+
+int main() {
+    I.sync_with_stdio(false);
+    I.tie(0);
+    ll i, n, c = 0;
+    I>>n;
+    V<ll> planets(n, 0);
+    F(i,0,n-2)
+    {
+        ll u, v;
+        I >> u >> v;
+        ++planets[u - 1];
+        ++planets[v - 1];
+    }
+    F(i,0,n-1)
+        if (planets[i] == 1)
+            ++c;
+    O << c << '\n';
+    return 0;
+}
