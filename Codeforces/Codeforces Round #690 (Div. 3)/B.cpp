be9109cdc61aac16ea043d7882133776874bcb67
hanzala-sohrab <sorb.hanzala@gmail.com>
2020-12-15 21:55:02 +0530

    Add Last Year's Substring

--- Codeforces/Codeforces Round #690 (Div. 3)/B.cpp
+++ Codeforces/Codeforces Round #690 (Div. 3)/B.cpp
@@ -1,34 +1,36 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+string possible(string s, int l) {
+	bool val = false;
+	if (l < 4)
+		val = false;
+	else if (s[0] == '2' and s[1] == '0' and s[2] == '2' and s[3] == '0')
+		val = true;
+	else if (s[l - 4] == '2' and s[l - 3] == '0' and s[l - 2] == '2' and s[l - 1] == '0')
+		val = true;
+	else if (s[0] == '2' and s[1] == '0' and s[l - 2] == '2' and s[l - 1] == '0')
+		val = true;
+	else if (s[0] == '2' and s[1] != '0' and s[l - 3] == '0' and s[l - 2] == '2' and s[l - 1] == '0')
+		val = true;
+	else if (s[0] == '2' and s[1] == '0' and s[2] == '2' and s[3] != '0' and s[l - 1] == '0')
+		val = true;
+	return val ? "YES\n" : "NO\n";
+}
+
 int main() {
 	cin.sync_with_stdio(false);
 	cin.tie(0);
-	int i, t;
+	int t;
 	cin >> t;
-	vector<string> ans(51, "-1");
-	for (i = 0; i < 10; ++i)
-		ans[i] = to_string(i);
-	for (i = 10; i < 18; ++i)
-		ans[i] = ans[i % 9] + ans[9];
-	for (i = 18; i < 25; ++i)
-		ans[i] = ans[i % 17] + ans[17];
-	for (i = 25; i < 31; ++i)
-		ans[i] = ans[i % 24] + ans[24];
-	for (i = 31; i < 36; ++i)
-		ans[i] = ans[i % 30] + ans[30];
-	for (i = 36; i < 40; ++i)
-		ans[i] = ans[i % 35] + ans[35];
-	for (i = 40; i < 43; ++i)
-		ans[i] = ans[i % 39] + ans[39];
-	for (i = 43; i < 45; ++i)
-		ans[i] = ans[i % 42] + ans[42];
-	ans[45] = "1" + ans[44];
 	while (t--)
 	{
-		int x;
-		cin >> x;
-		cout << ans[x] << '\n';
+		int n, i, j, k, l;
+		cin >> n;
+		string s;
+		cin >> s;
+		l = s.size();
+		cout << possible(s, l);
 	}
 	return 0;
 }
