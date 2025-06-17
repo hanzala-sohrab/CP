6638b7587d8bdc184e9074fdfe4e8c75f6571151
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-06-17 15:40:38 +0530

    Add 2114C.cpp

---
+++ Codeforces/2114C.cpp
@@ -0,0 +1,93 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define mmax(a, b, c) max(a, max(b, c))
+#define mmin(a, b, c) min(a, min(b, c))
+#define M 1000000007
+#define V vector
+#define F(a, b, c) for (a = b; a <= c; ++a)
+#define I cin
+#define O cout
+#define pb push_back
+#define all(x) x.begin(), x.end()
+#define ff first
+#define ss second
+#define umap unordered_map
+#define mp make_pair
+#define found(m, x) (m.find(x) != m.end())
+
+bool isPowerOfTwo(ll x)
+{
+    return x && (!(x & (x - 1)));
+}
+
+// https://cp-algorithms.com/algebra/binary-exp.html#toc-tgt-3
+ll binpow(ll a, ll b)
+{
+    a %= M;
+    ll res = 1;
+    while (b > 0)
+    {
+        if (b & 1)
+            res = res * a % M;
+        a = a * a % M;
+        b >>= 1;
+    }
+    return res;
+}
+
+// https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
+unsigned gcd(unsigned u, unsigned v)
+{
+    auto shift = __builtin_ctz(u | v);
+    u >>= __builtin_ctz(u);
+    do
+    {
+        v >>= __builtin_ctz(v);
+        if (u > v)
+            std::swap(u, v);
+    } while ((v -= u));
+    return u << shift;
+}
+
+int main()
+{
+    I.sync_with_stdio(false);
+    I.tie(0);
+    ll t, n, i, j;
+    I >> t;
+    while (t--)
+    {
+        I >> n;
+        set<ll> a;
+        F(i, 0, n - 1)
+        {
+            ll temp;
+            I >> temp;
+
+            a.insert(temp);
+        }
+
+        V<ll> b(all(a));
+
+        ll ans = 1;
+
+        ll m = b.size();
+
+        i = 0;
+        j = 1;
+
+        while (i < m and j < m)
+        {
+            if (b[i] + 1 < b[j])
+            {
+                ++ans;
+                i = j;
+            }
+            ++j;
+        }
+
+        O << ans << '\n';
+    }
+    return 0;
+}
