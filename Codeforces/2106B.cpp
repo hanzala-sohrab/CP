501eb1f68a1794b3603abe700f10a4bf19eb240d
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-06-18 12:36:40 +0530

    Add 2106B.cpp

---
+++ Codeforces/2106B.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long int
+#define F(a, b, c) for (a = b; a <= c; ++a)
+#define I cin
+#define O cout
+
+int main()
+{
+    I.sync_with_stdio(false);
+    I.tie(0);
+    ll t, n, x, i, j;
+    I >> t;
+    while (t--)
+    {
+        I >> n >> x;
+
+        j = 0;
+
+        F(i, 0, n - 1)
+        {
+            if (i == x)
+            {
+                continue;
+            }
+
+            O << i << ' ';
+            ++j;
+        }
+
+        if (j < n)
+        {
+
+            O << x;
+        }
+
+        O << '\n';
+    }
+    return 0;
+}
