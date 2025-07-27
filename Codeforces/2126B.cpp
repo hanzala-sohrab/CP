b0c8ba883392ba10bcb6879bb242a76271f060e1
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-07-27 19:52:57 +0530

    Add 2126B.cpp

--- Codeforces/2126B.cpp
+++ Codeforces/2126B.cpp
@@ -1,108 +1,26 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define mmax(a, b, c) max(a, max(b, c))
-#define mmin(a, b, c) min(a, min(b, c))
-#define M 1000000007
-#define V vector
-#define F(a, b, c) for (a = b; a <= c; ++a)
-#define I cin
-#define O cout
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define ff first
-#define ss second
-#define umap unordered_map
-#define mp make_pair
-#define found(m, x) (m.find(x) != m.end())
-
-bool isPowerOfTwo(ll x)
-{
-    return x && (!(x & (x - 1)));
-}
-
-bool isPrime(ll n)
-{
-    if (n < 2)
-        return false;
-    if (n == 2 || n == 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (ll i = 6; i * i <= n; i += 6)
-        if (n % (i - 1) == 0 || n % (i + 1) == 0)
-            return false;
-    return true;
-}
-
-// https://cp-algorithms.com/algebra/binary-exp.html#toc-tgt-3
-ll binpow(ll a, ll b)
-{
-    a %= M;
-    ll res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a % M;
-        a = a * a % M;
-        b >>= 1;
+int main() {
+  int t;
+  cin >> t;
+  while(t--) {
+    int n,k;
+    cin >> n >> k;
+    int a[n + 1];
+    for(int i = 1;i <= n;i++) cin >> a[i];
+    int ans = 0;
+    int cnt = 0;
+    for(int i = 1;i <= n;i++) {
+      if(a[i] == 1) {
+        ans+=(cnt + 1)/(k + 1);
+        cnt = 0;
+        continue;
+      }
+      else {
+        cnt++;
+      }
     }
-    return res;
-}
-
-// https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
-unsigned gcd(unsigned u, unsigned v)
-{
-    auto shift = __builtin_ctz(u | v);
-    u >>= __builtin_ctz(u);
-    do
-    {
-        v >>= __builtin_ctz(v);
-        if (u > v)
-            std::swap(u, v);
-    } while ((v -= u));
-    return u << shift;
-}
-
-int main()
-{
-    I.sync_with_stdio(false);
-    I.tie(0);
-    ll t, n, q, i, j, k;
-    I >> t;
-    while (t--)
-    {
-        I >> n >> k;
-        V<ll> a(n), b(n, LLONG_MAX);
-        F(i, 0, n - 1)
-        {
-            I >> a[i];
-        }
-        for (int i = n - 1; i >= 0; --i)
-        {
-            if (a[i] == 1)
-            {
-                b[i] = i;
-            }
-            else if (i < n - 1 && b[i + 1] != LLONG_MAX)
-            {
-                b[i] = b[i + 1];
-            }
-        }
-        q = 0;
-        F(i, 0, n - 1)
-        {
-            if (a[i])
-            {
-                continue;
-            }
-            if (k <= a[i])
-            {
-                ++q;
-                i += k - 1;
-            }
-        }
-        O << q << '\n';
-    }
-    return 0;
+    ans+=(cnt + 1)/(k + 1);
+    cout << ans << endl;
+  }
 }
