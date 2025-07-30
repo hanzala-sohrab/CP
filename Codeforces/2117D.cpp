e7ee8d83195cd5df544fe1137a7520dd255fb3da
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-07-30 12:14:17 +0530

    refactor: remove unused code

--- Codeforces/2117D.cpp
+++ Codeforces/2117D.cpp
@@ -1,113 +1,43 @@
 #include <bits/stdc++.h>
 using namespace std;
-#define ll long long int
-#define mmax(a, b, c) max(a, max(b, c))
-#define mmin(a, b, c) min(a, min(b, c))
-#define M 1000000007
 #define V vector
 #define F(a, b, c) for (a = b; a <= c; ++a)
 #define I cin
 #define O cout
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define ff first
-#define ss second
-#define umap unordered_map
-#define mp make_pair
-#define found(m, x) (m.find(x) != m.end())
-
-bool isPowerOfTwo(ll x)
-{
-    return x && (!(x & (x - 1)));
-}
-
-bool isPrime(ll n)
-{
-    if (n < 2)
-        return false;
-    if (n == 2 || n == 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (ll i = 6; i * i <= n; i += 6)
-        if (n % (i - 1) == 0 || n % (i + 1) == 0)
-            return false;
-    return true;
-}
-
-// https://cp-algorithms.com/algebra/binary-exp.html#toc-tgt-3
-ll binpow(ll a, ll b)
-{
-    a %= M;
-    ll res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a % M;
-        a = a * a % M;
-        b >>= 1;
-    }
-    return res;
-}
-
-// https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
-unsigned gcd(unsigned u, unsigned v)
-{
-    auto shift = __builtin_ctz(u | v);
-    u >>= __builtin_ctz(u);
-    do
-    {
-        v >>= __builtin_ctz(v);
-        if (u > v)
-            std::swap(u, v);
-    } while ((v -= u));
-    return u << shift;
-}
 
 int main()
 {
     I.sync_with_stdio(false);
     I.tie(0);
-    ll t, n, q, i, j, k;
+    int t;
     I >> t;
     while (t--)
     {
+        int i, n;
         I >> n;
-        V<ll> a(n);
+        V<int> a(n);
         F(i, 0, n - 1)
         {
             I >> a[i];
         }
 
-        ll y = (2 * a[0] - a[1]) / (n + 1);
+        int x = (2 * a[0] - a[1]) / (n + 1);
+        int z = a[1] - a[0];
+        int y = x + z;
 
-        if (y * (n + 1) != (2 * a[0] - a[1]) or y < 0)
+        if (x * (n + 1) != (2 * a[0] - a[1]) or x < 0 or y < 0)
         {
             O << "NO\n";
             continue;
         }
 
-        ll x = y + a[1] - a[0];
-
-        if (x < 0)
-        {
-            O << "NO\n";
-            continue;
-        }
-
-        ll z = x - y;
-
-        bool possible = true;
         F(i, 1, n - 1)
         {
             if ((a[i] - a[0]) % i != 0 or (a[i] - a[0]) / i != z)
-            {
-                possible = false;
                 break;
-            }
         }
 
-        O << (possible ? "YES\n" : "NO\n");
+        O << (i == n ? "YES\n" : "NO\n");
     }
     return 0;
 }
