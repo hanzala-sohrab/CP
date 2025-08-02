fb5b90cd8c7cc4a7f0041605a3e4b49b59eb2ffa
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-08-03 02:20:17 +0530

    Add 1979B.cpp

--- Codeforces/1979B.cpp
+++ Codeforces/1979B.cpp
@@ -1,68 +1,9 @@
 #include <bits/stdc++.h>
 using namespace std;
 #define ll long long int
-#define mmax(a, b, c) max(a, max(b, c))
-#define mmin(a, b, c) min(a, min(b, c))
-#define M 1000000007
-#define V vector
 #define F(a, b, c) for (a = b; a <= c; ++a)
 #define I cin
 #define O cout
-#define pb push_back
-#define all(x) x.begin(), x.end()
-#define ff first
-#define ss second
-#define umap unordered_map
-#define mp make_pair
-#define found(m, x) (m.find(x) != m.end())
-
-bool isPowerOfTwo(ll x)
-{
-    return x && (!(x & (x - 1)));
-}
-
-bool isPrime(ll n)
-{
-    if (n < 2)
-        return false;
-    if (n == 2 || n == 3)
-        return true;
-    if (n % 2 == 0 || n % 3 == 0)
-        return false;
-    for (ll i = 6; i * i <= n; i += 6)
-        if (n % (i - 1) == 0 || n % (i + 1) == 0)
-            return false;
-    return true;
-}
-
-// https://cp-algorithms.com/algebra/binary-exp.html#toc-tgt-3
-ll binpow(ll a, ll b)
-{
-    a %= M;
-    ll res = 1;
-    while (b > 0)
-    {
-        if (b & 1)
-            res = res * a % M;
-        a = a * a % M;
-        b >>= 1;
-    }
-    return res;
-}
-
-// https://lemire.me/blog/2013/12/26/fastest-way-to-compute-the-greatest-common-divisor/
-unsigned gcd(unsigned u, unsigned v)
-{
-    auto shift = __builtin_ctz(u | v);
-    u >>= __builtin_ctz(u);
-    do
-    {
-        v >>= __builtin_ctz(v);
-        if (u > v)
-            std::swap(u, v);
-    } while ((v -= u));
-    return u << shift;
-}
 
 int main()
 {
@@ -75,7 +16,6 @@ int main()
         int i, x, y;
         I >> x >> y;
 
-        ll ans = 1;
         F(i, 0, 31)
         {
             bool iThBitInX = x & (1 << i);
@@ -83,12 +23,10 @@ int main()
 
             if (iThBitInX != iThBitInY)
             {
-                ans = 1ll << i;
+                O<<(1ll << i) << '\n';
                 break;
             }
         }
-
-        O << ans << '\n';
     }
     return 0;
 }
