d8687b1853933830514ae639c64ad2849a88aaed
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-09-03 14:25:39 +0530

    Add 2131E.py

--- Codeforces/2131E.py
+++ Codeforces/2131E.py
@@ -1,4 +1,4 @@
-def is_possible() -> bool:
+def is_possible():
     n = int(input())
     a = list(map(int, input().split()))
     b = list(map(int, input().split()))
@@ -6,15 +6,16 @@ def is_possible() -> bool:
     if a[n - 1] != b[n - 1]:
         return False
 
-    for idx, val in enumerate(reversed(a)):
-        i = n - idx - 1
-        if i == n - 1 or val == b[i] or val ^ a[i + 1] == b[i] or val ^ b[i + 1] == b[i]:
+    for i in range(n - 2, -1, -1):
+        c = a[i] ^ b[i]
+        if a[i] == b[i] or c in [a[i + 1], b[i + 1]]:
             continue
-        return False
-    return True
 
+        return False
+
+    return True
 
 if __name__ == "__main__":
     t = int(input())
     for _ in range(t):
-        print("yes" if is_possible() else "no") 
+        print("yes" if is_possible() else "no")
