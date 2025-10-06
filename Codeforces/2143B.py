6f9c308da3e54295dfd8696f103a3dea54dbe908
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-10-06 23:21:26 +0530

    feat: add solution for Codeforces problem 2143B with test input

---
+++ Codeforces/2143B.py
@@ -0,0 +1,25 @@
+from typing import List
+
+def f(n: int, k: int, a: List[int], b: List[int]) -> int:
+    a.sort(reverse=True)
+    b.sort()
+
+    i = 0
+    j = 0
+    ans = 0
+    while j < k and i + b[j] - 1 < n:
+        i += b[j]
+        a[i - 1] = 0
+        j += 1
+
+    return sum(a)
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n, k = map(int, input().split())
+        a = list(map(int, input().split()))
+        b = list(map(int, input().split()))
+        print(f(n, k, a, b))
+
