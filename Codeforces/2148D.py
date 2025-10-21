3d1e5762bc45b08bb4d7b8fe67c772db505185a0
Hanzala Sohrab <hanzala.sohrab@iimjobs.com>
2025-10-21 11:27:34 +0530

    Add solution for problem 2148D

---
+++ Codeforces/2148D.py
@@ -0,0 +1,33 @@
+from typing import List
+
+def solve(n: int, a: List[int]) -> int:
+    a.sort(reverse=True)
+
+    odd_elements = [el for el in a if el % 2 == 1]
+    even_elements = [el for el in a if el % 2 == 0]
+
+    L = len(odd_elements)
+
+    if L == 0:
+        return 0
+
+    # two pointers
+    i, j = 0, L - 1
+
+    ans = sum(even_elements)
+
+    while j >= i:
+        ans += odd_elements[i]
+        i += 1
+        j -= 1
+
+    return ans
+
+
+if __name__ == "__main__":
+    t = int(input())
+    for _ in range(t):
+        n = int(input())
+        a = list(map(int, input().split()))
+        print(solve(n, a))
+
